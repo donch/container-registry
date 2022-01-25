062a47c26e6f5dacbf60c2bf6799e3e6d3a21c33
Hayley Swimelar <hswimelar@gitlab.com>
2022-01-25 21:18:40 +0000

    feat(handlers): update repository migration status during import

--- registry/handlers/import.go
+++ registry/handlers/import.go
@@ -10,6 +10,8 @@ import (
 	"github.com/docker/distribution/registry/api/errcode"
 	v2 "github.com/docker/distribution/registry/api/v2"
 	"github.com/docker/distribution/registry/datastore"
+	"github.com/docker/distribution/registry/datastore/models"
+	"github.com/docker/distribution/registry/internal/migration"
 	"github.com/docker/distribution/registry/storage"
 	ghandlers "github.com/gorilla/handlers"
 	"gitlab.com/gitlab-org/labkit/errortracking"
@@ -19,15 +21,16 @@ import (
 type importHandler struct {
 	*Context
 
-	datastore.RepositoryReader
+	datastore.RepositoryStore
+	preImport bool
 }
 
 // importDispatcher takes the request context and builds the
 // appropriate handler for handling import requests.
 func importDispatcher(ctx *Context, r *http.Request) http.Handler {
 	ih := &importHandler{
-		Context:          ctx,
-		RepositoryReader: datastore.NewRepositoryStore(ctx.App.db),
+		Context:         ctx,
+		RepositoryStore: datastore.NewRepositoryStore(ctx.App.db),
 	}
 
 	ihandler := ghandlers.MethodHandler{}
@@ -55,20 +58,13 @@ func (ih *importHandler) StartRepositoryImport(w http.ResponseWriter, r *http.Re
 	// Do not begin an import for a repository which has already been imported.
 	// TODO: When https://gitlab.com/gitlab-org/container-registry/-/issues/510 is
 	// complete, we should check the repository import status to determine if:
-	// * it's actually Migrated
+	// * it's actually Migrated - DONE
 	// * a pre-import is currently in progress
 	// * a pre-import failed
 	// * another import is currently in progress
 	// and communicate appropriately back to the client, as defined in the spec.
 	if dbRepo != nil {
-		// Horrible hack before !510 is finished to detect pre imported repositories.
-		tags, err := ih.Tags(ih.Context, dbRepo)
-		if err != nil {
-			ih.Errors = append(ih.Errors, errcode.FromUnknownError(err))
-			return
-		}
-
-		if len(tags) != 0 {
+		if dbRepo.MigrationStatus.OnDatabase() {
 			l.Info("repository already imported, skipping import")
 			w.WriteHeader(http.StatusOK)
 			return
@@ -93,17 +89,24 @@ func (ih *importHandler) StartRepositoryImport(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	preImport, err := getPreValue(r)
+	// TODO: We should have a specific error for bad query values.
+	ih.preImport, err = getPreValue(r)
 	if err != nil {
 		ih.Errors = append(ih.Errors, errcode.FromUnknownError(err))
+		return
 	}
 
-	l = l.WithFields(log.Fields{"pre_import": preImport})
+	l = l.WithFields(log.Fields{"pre_import": ih.preImport})
+
+	dbRepo, err = ih.createOrUpdateRepo(ih.Context, dbRepo)
+	if err != nil {
+		ih.Errors = append(ih.Errors, errcode.FromUnknownError(err))
+		return
+	}
 
 	go func() {
 		bts, err := storage.NewBlobTransferService(ih.App.driver, ih.App.migrationDriver)
 		if err != nil {
-			// TODO: We should have a specific error for bad query values.
 			ih.Errors = append(ih.Errors, errcode.FromUnknownError(err))
 		}
 
@@ -121,12 +124,7 @@ func (ih *importHandler) StartRepositoryImport(w http.ResponseWriter, r *http.Re
 		l := log.GetLogger(log.WithContext(ih.Context))
 		ctx = log.WithLogger(ctx, l)
 
-		if preImport {
-			err = importer.PreImport(ctx, ih.Repository.Named().Name())
-		} else {
-			err = importer.Import(ctx, ih.Repository.Named().Name())
-		}
-
+		ih.runImport(ctx, importer, dbRepo)
 		if err != nil {
 			l.WithError(err).Error("importing repository")
 			errortracking.Capture(err, errortracking.WithContext(ctx), errortracking.WithRequest(r))
@@ -134,6 +132,65 @@ func (ih *importHandler) StartRepositoryImport(w http.ResponseWriter, r *http.Re
 	}()
 
 	w.WriteHeader(http.StatusAccepted)
+}
+
+func (ih *importHandler) createOrUpdateRepo(ctx context.Context, dbRepo *models.Repository) (*models.Repository, error) {
+	// TODO: We should set the migration status in one step if the repository does not exist.
+	var err error
+	if dbRepo == nil {
+		dbRepo, err = ih.CreateByPath(ih.Context, ih.Repository.Named().Name())
+		if err != nil {
+			return dbRepo, fmt.Errorf("creating repository for import: %w", err)
+		}
+	}
+
+	if ih.preImport {
+		dbRepo.MigrationStatus = migration.RepositoryStatusPreImportInProgress
+	} else {
+		dbRepo.MigrationStatus = migration.RepositoryStatusImportInProgress
+	}
+
+	if err := ih.Update(ih.Context, dbRepo); err != nil {
+		return dbRepo, fmt.Errorf("updating migration status before import: %w", err)
+	}
+
+	return dbRepo, nil
+}
+
+func (ih *importHandler) runImport(ctx context.Context, importer *datastore.Importer, dbRepo *models.Repository) error {
+	if ih.preImport {
+		if err := importer.PreImport(ctx, dbRepo.Path); err != nil {
+			dbRepo.MigrationStatus = migration.RepositoryStatusPreImportFailed
+			if err := ih.Update(ctx, dbRepo); err != nil {
+				return fmt.Errorf("updating migration status after failed pre import: %w", err)
+			}
+
+			return err
+		}
+
+		dbRepo.MigrationStatus = migration.RepositoryStatusPreImportComplete
+		if err := ih.Update(ctx, dbRepo); err != nil {
+			return fmt.Errorf("updating migration status after successful pre import: %w", err)
+		}
+
+		return nil
+	}
+
+	if err := importer.Import(ctx, dbRepo.Path); err != nil {
+		dbRepo.MigrationStatus = migration.RepositoryStatusImportFailed
+		if err := ih.Update(ctx, dbRepo); err != nil {
+			return fmt.Errorf("updating migration status after failed import: %w", err)
+		}
+
+		return err
+	}
+
+	dbRepo.MigrationStatus = migration.RepositoryStatusImported
+	if err := ih.Update(ctx, dbRepo); err != nil {
+		return fmt.Errorf("updating migration status after successful import: %w", err)
+	}
+
+	return nil
 }
 
 // The API spec for this route only specifies 'true' or 'false', while
