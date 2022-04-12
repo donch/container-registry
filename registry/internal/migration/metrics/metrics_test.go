dc959bbd1bd5134db9afd973fecc6ef7250237be
João Pereira <jpereira@gitlab.com>
2022-04-12 16:43:58 +0100

    feat: add histograms for blob transfer durations and byte sizes

--- registry/internal/migration/metrics/metrics_test.go
+++ registry/internal/migration/metrics/metrics_test.go
@@ -93,3 +93,133 @@ registry_migration_layer_counts_count 2
 	err := testutil.GatherAndCompare(prometheus.DefaultGatherer, &expected, CountFullName)
 	require.NoError(t, err)
 }
+
+func TestBlobTransfer(t *testing.T) {
+	BlobTransfer(.1, 34234123, BlobTypeLayer)
+	BlobTransfer(0.25, 7754, BlobTypeConfig)
+	BlobTransfer(0.4, 5351258, BlobTypeUnknown)
+
+	var expected bytes.Buffer
+	expected.WriteString(`
+# HELP registry_migration_blob_transfer_duration_seconds A histogram of latencies for blob transfers.
+# TYPE registry_migration_blob_transfer_duration_seconds histogram
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.005"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.01"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.025"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.05"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.1"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.25"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="0.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="1"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="2.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="10"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="config",le="+Inf"} 1
+registry_migration_blob_transfer_duration_seconds_sum{blob_type="config"} 0.25
+registry_migration_blob_transfer_duration_seconds_count{blob_type="config"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.005"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.01"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.025"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.05"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.1"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.25"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="0.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="1"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="2.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="10"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="layer",le="+Inf"} 1
+registry_migration_blob_transfer_duration_seconds_sum{blob_type="layer"} 0.1
+registry_migration_blob_transfer_duration_seconds_count{blob_type="layer"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.005"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.01"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.025"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.05"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.1"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.25"} 0
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="0.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="1"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="2.5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="5"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="10"} 1
+registry_migration_blob_transfer_duration_seconds_bucket{blob_type="unknown",le="+Inf"} 1
+registry_migration_blob_transfer_duration_seconds_sum{blob_type="unknown"} 0.4
+registry_migration_blob_transfer_duration_seconds_count{blob_type="unknown"} 1
+# HELP registry_migration_blob_transfer_size_bytes A histogram of byte sizes for blob transfers.
+# TYPE registry_migration_blob_transfer_size_bytes histogram
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="524288"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="1.048576e+06"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="6.7108864e+07"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="1.34217728e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="2.68435456e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="5.36870912e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="1.073741824e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="2.147483648e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="3.221225472e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="4.294967296e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="5.36870912e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="6.442450944e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="7.516192768e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="8.589934592e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="9.663676416e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="1.073741824e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="2.147483648e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="3.221225472e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="4.294967296e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="5.36870912e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="config",le="+Inf"} 1
+registry_migration_blob_transfer_size_bytes_sum{blob_type="config"} 7754
+registry_migration_blob_transfer_size_bytes_count{blob_type="config"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="524288"} 0
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="1.048576e+06"} 0
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="6.7108864e+07"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="1.34217728e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="2.68435456e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="5.36870912e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="1.073741824e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="2.147483648e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="3.221225472e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="4.294967296e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="5.36870912e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="6.442450944e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="7.516192768e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="8.589934592e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="9.663676416e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="1.073741824e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="2.147483648e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="3.221225472e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="4.294967296e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="5.36870912e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="layer",le="+Inf"} 1
+registry_migration_blob_transfer_size_bytes_sum{blob_type="layer"} 3.4234123e+07
+registry_migration_blob_transfer_size_bytes_count{blob_type="layer"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="524288"} 0
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="1.048576e+06"} 0
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="6.7108864e+07"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="1.34217728e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="2.68435456e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="5.36870912e+08"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="1.073741824e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="2.147483648e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="3.221225472e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="4.294967296e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="5.36870912e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="6.442450944e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="7.516192768e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="8.589934592e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="9.663676416e+09"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="1.073741824e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="2.147483648e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="3.221225472e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="4.294967296e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="5.36870912e+10"} 1
+registry_migration_blob_transfer_size_bytes_bucket{blob_type="unknown",le="+Inf"} 1
+registry_migration_blob_transfer_size_bytes_sum{blob_type="unknown"} 5.351258e+06
+registry_migration_blob_transfer_size_bytes_count{blob_type="unknown"} 1
+`)
+	durationFullName := fmt.Sprintf("%s_%s_%s", metrics.NamespacePrefix, subsystem, blobTransferDurationName)
+	sizeFullName := fmt.Sprintf("%s_%s_%s", metrics.NamespacePrefix, subsystem, blobTransferSizeName)
+
+	err := testutil.GatherAndCompare(prometheus.DefaultGatherer, &expected, durationFullName, sizeFullName)
+	require.NoError(t, err)
+}
