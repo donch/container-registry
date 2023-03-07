a184751f96b586d7bbabbee5dd917034f60f36d2
João Pereira <jpereira@gitlab.com>
2023-03-07 15:21:40 +0000

    fix: revert temporarily log query duration for debugging

--- registry/datastore/repository.go
+++ registry/datastore/repository.go
@@ -409,8 +409,6 @@ func (s *repositoryStore) FindByPath(ctx context.Context, path string) (*models.
 		return cached, nil
 	}
 
-	l := log.GetLogger(log.WithContext(ctx))
-
 	defer metrics.InstrumentQuery("repository_find_by_path")()
 	q := `SELECT
 			id,
@@ -428,24 +426,12 @@ func (s *repositoryStore) FindByPath(ctx context.Context, path string) (*models.
 			path = $1
 			AND deleted_at IS NULL` // temporary measure for the duration of https://gitlab.com/gitlab-org/container-registry/-/issues/570
 
-	t := time.Now()
 	row := s.db.QueryRowContext(ctx, q, path)
 
 	r, err := scanFullRepository(row)
 	if err != nil {
 		return r, err
 	}
-
-	var found bool
-	if r != nil {
-		found = true
-	}
-	l.WithFields(log.Fields{
-		"duration_ms": time.Since(t).Milliseconds(),
-		"name":        "repository_find_by_path",
-		"repository":  path,
-		"found":       found,
-	}).Info("query")
 
 	s.cache.Set(ctx, r)
 
@@ -909,8 +895,6 @@ func (s *repositoryStore) FindManifestByDigest(ctx context.Context, r *models.Re
 
 // FindManifestByTagName finds a manifest by tag name within a repository.
 func (s *repositoryStore) FindManifestByTagName(ctx context.Context, r *models.Repository, tagName string) (*models.Manifest, error) {
-	l := log.GetLogger(log.WithContext(ctx))
-
 	defer metrics.InstrumentQuery("repository_find_manifest_by_tag_name")()
 	q := `SELECT
 			m.id,
@@ -939,27 +923,9 @@ func (s *repositoryStore) FindManifestByTagName(ctx context.Context, r *models.R
 			AND m.repository_id = $2
 			AND t.name = $3`
 
-	t := time.Now()
 	row := s.db.QueryRowContext(ctx, q, r.NamespaceID, r.ID, tagName)
 
-	m, err := scanFullManifest(row)
-	if err != nil {
-		return m, err
-	}
-
-	var found bool
-	if m != nil {
-		found = true
-	}
-	l.WithFields(log.Fields{
-		"duration_ms": time.Since(t).Milliseconds(),
-		"name":        "repository_find_manifest_by_tag_name",
-		"repository":  r.Path,
-		"tag_name":    tagName,
-		"found":       found,
-	}).Info("query")
-
-	return m, nil
+	return scanFullManifest(row)
 }
 
 // Blobs finds all blobs associated with the repository.
